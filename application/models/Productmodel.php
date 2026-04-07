53853697eee1269be7a5e777b21569d57506b26f
athirablindtex <athirablindtex@gmail.com>
2026-04-07 12:21:41 +0400

    company id added in product filter

--- application/models/Productmodel.php
+++ application/models/Productmodel.php
@@ -411,17 +411,19 @@ private function base_query()
 	{
 
 	 $select_company_id = $this->input->get('company_id');
-	 echo $select_company_id;exit;
+	
 
-    // if (!empty($select_company_id)) {
-    //     $company_id = get_company_id_or_null($select_company_id);
+    if (!empty($select_company_id)) {
+        $company_id = get_company_id_or_null($select_company_id);
+		echo "Company ID: " . ($company_id === null ? 'NULL' : $company_id);
+		exit; // Debugging line
 
-    //     if ($company_id === NULL) {
-    //         $this->db->where('a.company_id IS NULL', null, false);
-    //     } else {
-    //         $this->db->where('a.company_id', $company_id);
-    //     }
-    // }
+        // if ($company_id === NULL) {
+        //     $this->db->where('a.company_id IS NULL', null, false);
+        // } else {
+        //     $this->db->where('a.company_id', $company_id);
+        // }
+    }
 
         
 		if ($this->input->get('product_type')) {
