294267777bc5c93477cd95cd60dce6d3e2892ca2
athirablindtex <athirablindtex@gmail.com>
2026-04-07 17:29:04 +0400

    company id added in product filter

--- application/controllers/admin/Producttype.php
+++ application/controllers/admin/Producttype.php
@@ -2,8 +2,8 @@
 	exit('No direct script allowed');
 }
 
-ini_set(('display_errors'), 1);
-error_reporting(E_ALL);
+// ini_set(('display_errors'), 1);
+// error_reporting(E_ALL);
 
 class Producttype extends MY_Controller
 {
