48f15eaceadccaa4079ad4358d61bea20a47cd60
athirablindtex <athirablindtex@gmail.com>
2026-04-07 10:21:34 +0400

    company page updation

--- application/models/Productmodel.php
+++ application/models/Productmodel.php
@@ -244,8 +244,8 @@ class Productmodel extends CI_Model
 
 		$this->db->where('deleted_at', Null);
 		$dt = $this->gets_data()->result_array();
-		print_r($this->db->last_query());
-		exit; // Debugging line to check the generated SQL query
+		// print_r($this->db->last_query());
+		// exit; // Debugging line to check the generated SQL query
 
 
 
