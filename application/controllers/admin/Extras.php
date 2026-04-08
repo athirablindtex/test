bb9bbe3568c0c0cf58cc61b4649abe17f7c3c876
athirablindtex <athirablindtex@gmail.com>
2026-04-08 16:42:27 +0400

    company id added in product filter

--- application/controllers/admin/Extras.php
+++ application/controllers/admin/Extras.php
@@ -798,6 +798,7 @@ public function get_margin_excel_data()
         $module_model = $this->module_model;
         $this->db->where('parent', 0)->select('id,name');
 		$this->db->where('company_id', $company_id);
+		
         $data['product_types'] = $this->producttypemodel->gets_data()->result();
         $this->db->where('parent', 0)->select('id,name')->from('extras');
         $data['extras_type'] = $this->db->get()->result();
