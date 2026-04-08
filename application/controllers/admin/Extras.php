c70632f228910f61a810cbd3851df1c18448cf1e
athirablindtex <athirablindtex@gmail.com>
2026-04-08 16:41:19 +0400

    company  search added in extras

--- application/controllers/admin/Extras.php
+++ application/controllers/admin/Extras.php
@@ -787,6 +787,7 @@ public function get_margin_excel_data()
         $data['content'] = 'admin/' . $view_folder . '/extras_margin';
         $data['redirect'] = site_url() . $data['content'];
         $data['edit'] = 0;
+		$company_id = $this->input->get('company_id');
 		$company_ids = $this->companies ?? [];
          if (!empty($company_ids)) {
         $this->db->where_in('id', $company_ids);
@@ -796,6 +797,7 @@ public function get_margin_excel_data()
         // $data['product_types'] = $this->producttypemodel->gets_data()->result();
         $module_model = $this->module_model;
         $this->db->where('parent', 0)->select('id,name');
+		$this->db->where('company_id', $company_id);
         $data['product_types'] = $this->producttypemodel->gets_data()->result();
         $this->db->where('parent', 0)->select('id,name')->from('extras');
         $data['extras_type'] = $this->db->get()->result();
