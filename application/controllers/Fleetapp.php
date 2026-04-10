9816196bb02bfd851ffcd8317ef41ed2ee7fb6e8
athirablindtex <athirablindtex@gmail.com>
2026-04-10 13:44:05 +0400

    company id added in product filter

--- application/controllers/Fleetapp.php
+++ application/controllers/Fleetapp.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 class FleetApp extends CI_Controller
 {
     public function __construct()
