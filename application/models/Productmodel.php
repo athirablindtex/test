fe68c457dca0c10f45c3bbe0c3c810be2264a3d3
athirablindtex <athirablindtex@gmail.com>
2026-04-16 08:53:38 +0400

    payemnt updations

--- application/models/Productmodel.php
+++ application/models/Productmodel.php
@@ -246,7 +246,11 @@ class Productmodel extends CI_Model
           $company_id = get_company_id_or_null($select_company_id);
 		 
 	
-			$this->db->where('company_id', $company_id);
+	if ($company_id === null) {
+    $this->db->where('company_id IS NULL', null, false);
+} else {
+    $this->db->where('company_id', $company_id);
+}
 		
 
 
